d1c3cf4d4006a2a296b693c88ba7a495ae9f57cc
rafi-disys <124764810+rafi-disys@users.noreply.github.com>
2023-08-25 18:43:06 +0530

    messedup

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const multer = require('multer'); //middleware package that handles file uploads
+const sql = require('mssql');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Set up database configuration
+const dbConfig = {
+    user: 'your-username',
+    password: 'your-password',
+    server: 'your-server.database.windows.net',
+    database: 'your-database',
+    options: {
+        encrypt: true // Enable encryption for Azure SQL
+    }
+};
+
+// Set up multer for handling file uploads
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+// Set up server routes
+app.post('/upload', upload.single('file'), async (req, res) => {
+    try {
+        // Access the uploaded file using req.file
+        const uploadedFile = req.file;
+
+        // Connect to the database
+        const pool = await sql.connect(dbConfig);
+
+        // Insert data into the database
+        const query = `
+            INSERT INTO UploadedFiles (OriginalFilename, UploadTimestamp)
+            VALUES (@originalFilename, @uploadTimestamp);
+        `;
+        await pool.request()
+            .input('originalFilename', sql.NVarChar, uploadedFile.originalname)
+            .input('uploadTimestamp', sql.DateTime, new Date())
+            .query(query);
+
+        res.status(200).send('File uploaded and data saved.');
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).send('An error occurred.');
+    }
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
