e9f37cde035621c2634d8a4ae0f22f1b247382f2
rafi-disys <124764810+rafi-disys@users.noreply.github.com>
2023-08-31 13:41:44 +0000

    used blob storage for files

--- app.js
+++ app.js
@@ -1,66 +1,52 @@
-const express = require('express'); // for 
-const path = require('path');
-const multer = require('multer'); //middleware package that handles file uploads
-const sql = require('mssql');
+const express = require("express");
+const path = require("path");
+const { v1: uuidv1 } = require("uuid");
+const multer = require("multer"); // Middleware package that handles file uploads
+const { BlobServiceClient } = require("@azure/storage-blob");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Serve static assets from the "public" directory
-app.use(express.static(path.join(__dirname, 'public')));
+// Set up storage configuration for Azure Blob Storage
+const blobServiceClient = BlobServiceClient.fromConnectionString(
+  "DefaultEndpointsProtocol=https;AccountName=sharesafely;AccountKey=6Ak/J8LXJJfboiEUuxpxPu88r//CK7WBvb8LDu8FFqme+Xnr7LIsSZg6K4lmO4J20jBbX3OddO8/+AStoeMkxw==;EndpointSuffix=core.windows.net"
+);
+const containerName = "filecontainer";
+const containerClient = blobServiceClient.getContainerClient(containerName);
 
-// Set up database configuration
-const dbConfig = {
-    user: 'your-username',
-    password: 'your-password',
-    server: 'your-server.database.windows.net',
-    database: 'your-database',
-    options: {
-        encrypt: true // Enable encryption for Azure SQL
-    }
-};
+// Serve static assets from the "public" directory
+app.use(express.static(path.join(__dirname, "public")));
 
 // Set up multer for handling file uploads
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
 // Set up server routes
-app.post('/upload', upload.single('file'), async (req, res) => {
-    try {
-        // Access the uploaded file using req.file
-        const uploadedFile = req.file;
-        
-        if (uploadedFile) {
-            // Log the buffer content to the console
-            console.log(uploadedFile.buffer.toString('utf-8'));
+app.post("/upload", upload.single("file"), async (req, res) => {
+  try {
+    // Access the uploaded file using req.file
+    const uploadedFile = req.file;
 
-            // Your other processing logic here...
+    if (uploadedFile) {
+      // Get a reference to a blob
+      const blobName = uploadedFile.originalname;
+      const blockBlobClient = containerClient.getBlockBlobClient(blobName);
 
-            res.status(200).send('File uploaded and data saved.');
-        } else {
-            res.status(400).send('No file uploaded.');
-        }
-        // Connect to the database
-        const pool = await sql.connect(dbConfig);
+      // Upload the file buffer to Azure Blob Storage
+      const buffer = uploadedFile.buffer;
+      await blockBlobClient.uploadData(buffer, buffer.length);
 
-        // Insert data into the database
-        const query = `
-            INSERT INTO UploadedFiles (OriginalFilename, UploadTimestamp)
-            VALUES (@originalFilename, @uploadTimestamp);
-        `;
-        await pool.request()
-            .input('originalFilename', sql.NVarChar, uploadedFile.originalname)
-            .input('uploadTimestamp', sql.DateTime, new Date())
-            .query(query);
-
-        res.status(200).send('File uploaded and data saved.');
-    } catch (error) {
-        console.error('Error:', error);
-        res.status(500).send('An error occurred.');
+      res.status(200).send("File uploaded to Azure Blob Storage.");
+    } else {
+      res.status(400).send("No file uploaded.");
     }
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).send("An error occurred.");
+  }
 });
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
